f02fc626884f9c73b7bced012dc7cce5c9303531
Mariana Leite <fbr.mariana@gmail.com>
2020-07-26 23:34:41 -0300

    Melhorias

--- www/resources/views/index.blade.php
+++ www/resources/views/index.blade.php
@@ -2,58 +2,59 @@
 
 @section('content')
 
-<h1 class ="text-center">Cadastro de Livros</h1><hr>
+<h1 class="text-center">Cadastro de Livros</h1>
+<hr>
 <div class="text-center mt-3 mb-4">
 
-<a href="{{url('books/create')}}">
-      <button class="btn btn-success">Cadastrar<button>
-      </a>
+  <a href="{{url('books/create')}}">
+    <button class="btn btn-success"> Cadastrar </button>
+  </a>
 
 </div>
 
 <div class="col-8 m-auto">
   @csrf
   <table class="table text-center">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Titulo</th>
-      <th scope="col">Autor</th>
-      <th scope="col">Preço</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Titulo</th>
+        <th scope="col">Autor</th>
+        <th scope="col">Preço</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
 
-  @foreach($book as $books)
-  @php
-       $user=$books->find($books->id)->relUsers;
-  @endphp
-  <tr>
-      <th scope="row">{{$books->id}}</th>
-      <td>{{$books->title}}</td>
-      <td>{{$user->name}}</td>
-      <td>{{$books->price}}</td>
-      <td>
+      @foreach($book as $books)
+      @php
+      $user=$books->find($books->id)->relUsers;
+      @endphp
+      <tr>
+        <th scope="row">{{$books->id}}</th>
+        <td>{{$books->title}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$books->price}}</td>
+        <td>
           <a href="{{url("books/$books->id")}}">
-              <button class="btn btn-dark">Visualizar<button>
-      </a>
+            <button class="btn btn-dark">Visualizar</button>
+          </a>
 
-      <a href="{{url("books/$books->id/edit")}}">
-            <button class="btn btn-primary">Editar<button>
-      </a>
+          <a href="{{url("books/$books->id/edit")}}">
+            <button class="btn btn-primary">Editar</button>
+          </a>
 
-      <a href="{{url("books/$books->id")}}" class="js-del">
-    <button class="btn btn-danger">Deletar</button>
-</a>
-   
-  @endforeach
-  
-    
+          <a href="{{url("books/$books->id")}}" class="js-del">
+            <button class="btn btn-danger">Deletar</button>
+          </a>
+
+          @endforeach
+
+
+    </tbody>
+
+  </table>
   </tbody>
-
-</table>
-</tbody>
 </div>
 
-@endsection 
+@endsection
